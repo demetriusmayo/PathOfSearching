7928941e896e4eab469c2ec0141cde941c7c549f
WORK <WORK@WORKPC>
2019-02-13 15:54:13 +0200

    idea

--- Demo/Form1.cs
+++ Demo/Form1.cs
@@ -16,5 +16,10 @@ namespace Demo
         {
             InitializeComponent();
         }
+        // Кнопка для обработки события нажатия
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Utils.OneItem();
+        }
     }
 }
---
+++ Demo/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NLua;
+
+namespace Demo
+{
+    class Utils
+    {
+        internal static void OneItem()
+        {
+            var state = new Lua ();
+            state.DoString("y = 10 + x*(5 + 2)");
+            var y = state["y"];
+            return;
+        }
+
+    }
+}
